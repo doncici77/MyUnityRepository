52d0e51f9f368b6bc2020f539dcfa9d040d246e6
doncici77 <pkh2956@naver.com>
2025-02-26 13:35:37 +0900

    백그라운드 코드 구현

--- My project 2025_02_26/Assets/Scripts/Background.cs
+++ My project 2025_02_26/Assets/Scripts/Background.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 
+// 배경 스크롤 기능을 만들 겁니다
+// 확인한 것 : 머티리얼 offet을 건드렸더니 이미지가 밀리더라
+
+// 필요한 것 : 머티리얼, 스크롤링 속도
+// 어떻게 진행할 것인가? 위로? 아래로? 옆으로?
+
 public class Background : MonoBehaviour
 {
+    public Material backgroundMaterial;
+    public float scrollSpeed = 0.2f;
 
-    void Start()
+    private void Update()
     {
-        
-    }
+        Vector2 dir = Vector2.up;
 
-    void Update()
-    {
-        
+        backgroundMaterial.mainTextureOffset += dir * scrollSpeed * Time.deltaTime;
     }
 }
